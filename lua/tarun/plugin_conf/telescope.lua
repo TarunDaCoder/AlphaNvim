9f7acf42feb1d758b0e9f19569228089e39b9162
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-30 21:52:00 +0530

    use telescope with trouble

--- lua/tarun/plugin_conf/telescope.lua
+++ lua/tarun/plugin_conf/telescope.lua
@@ -13,3 +13,17 @@ require("telescope").setup({
 })
 
 require("telescope").load_extension("ui-select")
+
+local open_with_trouble = require("trouble.sources.telescope").open
+
+
+local telescope = require("telescope")
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = { ["<c-t>"] = open_with_trouble },
+      n = { ["<c-t>"] = open_with_trouble },
+    },
+  },
+})
