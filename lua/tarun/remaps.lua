c9d13a361f0e36074c8e7ad7e9113b4f7e86a8c7
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-30 18:40:09 +0530

    esc better

--- lua/tarun/remaps.lua
+++ lua/tarun/remaps.lua
@@ -1,4 +1,3 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
-
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
