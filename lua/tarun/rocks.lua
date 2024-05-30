ad290dfbdb051395e086db51ea8056d39a31014b
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-30 18:40:12 +0530

    idk

--- lua/tarun/rocks.lua
+++ lua/tarun/rocks.lua
@@ -2,15 +2,15 @@
      rocks_path = vim.env.HOME .. "/.local/share/nvim/rocks",
      luarocks_binary = vim.env.HOME .. "/.local/share/nvim/rocks/bin/luarocks",
  }
- 
+
  vim.g.rocks_nvim = rocks_config
- 
+
  local luarocks_path = {
      vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
      vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
  }
  package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
- 
+
  local luarocks_cpath = {
      vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
      vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
@@ -21,5 +21,5 @@
      vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dll"),
  }
  package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
- 
+
  vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
