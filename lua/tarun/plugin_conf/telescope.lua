6374bbb2f2134539b344724a779438226dee04bb
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-30 18:40:03 +0530

    use telescope-ui-select

--- lua/tarun/plugin_conf/telescope.lua
+++ lua/tarun/plugin_conf/telescope.lua
@@ -3,3 +3,13 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("telescope").setup({
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown{}
+        }
+    }
+})
+
+require("telescope").load_extension("ui-select")
