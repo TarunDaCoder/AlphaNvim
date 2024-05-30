3d22833b073919b032a5cf9a751b8593be26b1eb
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-30 18:39:48 +0530

    use dashboard and install a few lsp plugins

--- lua/tarun/plugin_conf/lspconf.lua
+++ lua/tarun/plugin_conf/lspconf.lua
@@ -1,22 +1,11 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls" }
+  ensure_installed = { "lua_ls", "cssls", "eslint", "emmet_ls", "grammarly", "html", "quick_lint_js", "pyright", "tsserver", }
 })
-
-local on_attach = function(_, _)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-  vim.keymap.set("n", "gr", require("telescope.builtin").lsp_preferences, {})
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("lspconfig").lua_ls.setup {
-  on_attach = on_attach,
   capabilties = capabilities,
   settings = {
     Lua = {
@@ -32,5 +21,18 @@ require("lspconfig").lua_ls.setup {
     },
   },
 }
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+
+require("lspconfig").pyright.setup({})
+require("lspconfig").emmet_ls.setup({})
+require("lspconfig").cssls.setup({})
+require("lspconfig").eslint.setup({})
+require("lspconfig").grammarly.setup({})
+require("lspconfig").quick_lint_js.setup({})
+require("lspconfig").html.setup({})
+require("lspconfig").tsserver.setup({})
 
 require("lspsaga").setup({})
