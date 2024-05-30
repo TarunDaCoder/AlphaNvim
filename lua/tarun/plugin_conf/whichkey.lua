9c36ef3194a521db7b9ce7b4a28b7656737a9221
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-30 22:54:42 +0530

    add whichkey

---
+++ lua/tarun/plugin_conf/whichkey.lua
@@ -0,0 +1,13 @@
+local wk = require("which-key")
+
+wk.setup({
+    window = {
+        border = 'double',
+        winblend = 30,
+    },
+    icons = {
+        breadcrumb = '➜',
+        separator = '»',
+        group = '+',
+    },
+})
