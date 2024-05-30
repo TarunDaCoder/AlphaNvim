5aebe846327bba6a8782af0242a7a1317882cc36
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-30 21:51:46 +0530

    use tab complete for cmp

--- lua/tarun/plugin_conf/completions.lua
+++ lua/tarun/plugin_conf/completions.lua
@@ -1,179 +1,120 @@
-local status_ok, cmp = pcall(require, 'cmp')
-if not status_ok then
-	return
-end
-
-local lsnip_status_ok, luasnip = pcall(require, 'luasnip')
-if not lsnip_status_ok then
-	return
-end
-
-require('luasnip/loaders/from_vscode').lazy_load()
-
+local cmp = require'cmp'
+local luasnip = require("luasnip")
 local check_backspace = function()
 	local col = vim.fn.col('.') - 1
 	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
 end
 
---   פּ ﯟ   some other good icons
--- local kind_icons = {
---   Text = "",
---   Method = "m",
---   Function = "",
---   Constructor = "",
---   Field = "",
---   Variable = "",
---   Class = "",
---   Interface = "",
---   Module = "",
---   Property = "",
---   Unit = "",
---   Value = "",
---   Enum = "",
---   Keyword = "",
---   Snippet = "",
---   Color = "",
---   File = "",
---   Reference = "",
---   Folder = "",
---   EnumMember = "",
---   Constant = "",
---   Struct = "",
---   Event = "",
---   Operator = "",
---   TypeParameter = "",
--- }
--- find more here: https://www.nerdfonts.com/cheat-sheet
-
 local border = {
-	'╔',
-	'═',
-	'╗',
-	'║',
-	'╝',
-	'═',
-	'╚',
-	'║',
+    { "╭", "CmpBorder" },
+    { "─", "CmpBorder" },
+    { "╮", "CmpBorder" },
+    { "│", "CmpBorder" },
+    { "╯", "CmpBorder" },
+    { "─", "CmpBorder" },
+    { "╰", "CmpBorder" },
+    { "│", "CmpBorder" },
 }
 
 cmp.setup({
-	completion = {
-		border = border,
-	},
-	window = {
-		documentation = { border = 'rounded' },
-		completion = { border = 'rounded' },
-	},
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body) -- For `luasnip` users.
-		end,
-	},
-	mapping = {
-		['<C-k>'] = cmp.mapping.select_prev_item(),
-		['<C-j>'] = cmp.mapping.select_next_item(),
-		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
-		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
-		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-		['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		['<C-e>'] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		-- Accept currently selected item. If none selected, `select` first item.
-		-- Set `select` to `false` to only confirm explicitly selected items.
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif check_backspace() then
-				fallback()
-			else
-				fallback()
-			end
-		end, {
-			'i',
-			's',
-		}),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {
-			'i',
-			's',
-		}),
-		['<C-J>'] = cmp.mapping(function(fallback)
-			cmp.mapping.abort()
-			local copilot_keys = vim.fn['copilot#Accept']()
-			if copilot_keys ~= '' then
-				vim.api.nvim_feedkeys(copilot_keys, 'i', true)
-			else
-				fallback()
-			end
-		end, {
-			'i',
-			's',
-		}),
-	},
-	formatting = {
-		-- fields = { 'kind', 'abbr', 'menu' },
-		format = require('lspkind').cmp_format({
-			with_text = false,
-			menu = {
-				buffer = '[Buf]',
-				nvim_lsp = '[LSP]',
-				nvim_lua = '[Lua]',
-				path = '[Path]',
-				neorg = '[Neorg]',
-				luasnip = '[LuaSnip]',
-			},
-		}),
-
-		-- format = function(entry, vim_item)
-		--   -- Kind icons
-		--   vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-		--   -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-		--   vim_item.menu = ({
-		--   luasnip = "",
-		--   })[entry.source.name]
-		--   return vim_item
-		-- end,
-	},
-	sources = {
-		{ name = 'neorg' },
-		{ name = 'luasnip' },
-		{ name = 'buffer' },
-		{ name = 'path' },
-		{ name = 'nvim_lua' },
-		{ name = 'nvim_lsp' },
-	},
-	confirm_opts = {
-		behavior = cmp.ConfirmBehavior.Replace,
-		select = false,
-	},
-	experimental = {
-		ghost_text = true,
-		native_menu = false,
-	},
+  snippet = {
+  expand = function(args)
+    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+  end,
+  },
+  window = {
+    completion = {
+        border = border,
+        scrollbar = "║",
+    },
+    documentation = {
+        border = border,
+        scrollbar = "║",
+    }
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+	-- Accept currently selected item. If none selected, `select` first item.
+	-- Set `select` to `false` to only confirm explicitly selected items.
+	['<CR>'] = cmp.mapping.confirm({ select = true }),
+	['<Tab>'] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		elseif luasnip.expandable() then
+			luasnip.expand()
+		elseif luasnip.expand_or_jumpable() then
+			luasnip.expand_or_jump()
+		elseif check_backspace() then
+			fallback()
+		else
+			fallback()
+		end
+	end, {
+		'i',
+		's',
+	}),
+	['<S-Tab>'] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		elseif luasnip.jumpable(-1) then
+			luasnip.jump(-1)
+		else
+			fallback()
+		end
+	end, {
+		'i',
+		's',
+	}),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
+  })
 })
 
+  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+  -- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+require("cmp_git").setup()
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+  { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-	sources = {
-		{ name = 'cmdline' },
-	},
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
 })
 
-cmp.setup.cmdline('/', {
-	sources = {
-		{ name = 'buffer' },
-	},
-})
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+    })
+  }
+}
