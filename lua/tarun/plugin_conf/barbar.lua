9c74aa038d2cbef3e256d103679e1ea68c498e06
TarunDaCoder <tarunsnair04@gmail.com>
2024-05-31 23:20:41 +0530

    wezterm didn't support that icon

---
+++ lua/tarun/plugin_conf/barbar.lua
@@ -0,0 +1,9 @@
+local barbar = require("barbar")
+
+vim.g.barbar_auto_setup = false
+
+barbar.setup({
+    icons = {
+        button = "X"
+    }
+})
--- lua/tarun/plugin_conf/dash.lua
+++ lua/tarun/plugin_conf/dash.lua
@@ -18,7 +18,7 @@ dashboard.section.buttons.val = {
 	dashboard.button('f', '' .. ' Find file', ':Telescope find_files <CR>'),
 	dashboard.button('e', '' .. ' New file', ':ene <BAR> startinsert <CR>'),
 	dashboard.button('p', '' .. ' Find project', ':Telescope project project <CR>'),
-	dashboard.button('r', '' .. ' Recent files', ':Telescope oldfiles <CR>'),
+	dashboard.button('r', '' .. ' Recent files', ':Telescope oldfiles <CR>'),
 	dashboard.button('t', '' .. ' Find text', ':Telescope live_grep <CR>'),
 	dashboard.button('s', '' .. ' Find Session', ':Telescope sessions_picker <CR>'),
 	dashboard.button('c', '' .. ' Config', ':e ~/.config/nvim/init.lua <CR>'),
