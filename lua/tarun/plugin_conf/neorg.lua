d9818f8461b69e47d3101da55889c099a7c40d5d
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-01 09:49:23 +0530

    change neorg config and remove swapfiles

--- lua/tarun/plugin_conf/neorg.lua
+++ lua/tarun/plugin_conf/neorg.lua
@@ -1,14 +1,52 @@
-require("neorg").setup({
+local neorg = require("neorg")
+neorg.setup({
     load = {
-        ["core.defaults"] = {},
-        ["core.concealer"] = {},
-        ["core.dirman"] = {
+        ['core.defaults'] = {},
+        ['core.norg.completion'] = {
             config = {
-                workspaces = {
-                    notes = "~/neorg",
-                },
-                default_workspace = "notes",
+                engine = 'nvim-cmp',
             },
         },
+        ['core.norg.concealer'] = {
+            config = {
+                markup_preset = 'conceal',
+                icon_preset = 'diamond',
+                icons = {
+                    marker = {
+                        icon = '󰁘 ',
+                    },
+                    todo = {
+                        enable = true,
+                        pending = {
+                            -- icon = ""
+                            icon = '',
+                        },
+                        uncertain = {
+                            icon = '?',
+                        },
+                        urgent = {
+                            icon = '',
+                        },
+                        on_hold = {
+                            icon = '󰏦',
+                        },
+                        cancelled = {
+                            icon = '',
+                        },
+                    },
+                },
+            },
+        },
+        ['core.norg.dirman'] = {
+            config = {
+                workspaces = {
+                    bobvim_todos = '~/.config/nvim/',
+                    neorg = '~/.norg/',
+                },
+                autodetect = true,
+                autochdir = true,
+            },
+        },
+        ['core.integrations.telescope'] = {},
     },
 })
