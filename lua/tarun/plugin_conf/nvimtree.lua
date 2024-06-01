aa544f604a8e293a3b61620f9d0726d5f14c0ab8
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-01 23:07:21 +0530

    format

--- lua/tarun/plugin_conf/nvimtree.lua
+++ lua/tarun/plugin_conf/nvimtree.lua
@@ -10,39 +10,44 @@ require('nvim-tree').setup()
 
 -- OR setup with some options
 require('nvim-tree').setup({
-    sort = {
-        sorter = 'case_sensitive',
-    },
-    view = {
-        width = 30,
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
+	sort = {
+		sorter = 'case_sensitive',
+	},
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
 })
 
 -- auto close
 local function is_modified_buffer_open(buffers)
-    for _, v in pairs(buffers) do
-        if v.name:match('NvimTree_') == nil then
-            return true
-        end
-    end
-    return false
+	for _, v in pairs(buffers) do
+		if v.name:match('NvimTree_') == nil then
+			return true
+		end
+	end
+	return false
 end
 
 vim.api.nvim_create_autocmd('BufEnter', {
-    nested = true,
-    callback = function()
-        if
-            #vim.api.nvim_list_wins() == 1
-            and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil
-            and is_modified_buffer_open(vim.fn.getbufinfo({ bufmodified = 1 })) == false
-        then
-            vim.cmd('quit')
-        end
-    end,
+	nested = true,
+	callback = function()
+		if
+			#vim.api.nvim_list_wins() == 1
+			and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil
+			and is_modified_buffer_open(vim.fn.getbufinfo({ bufmodified = 1 })) == false
+		then
+			vim.cmd('quit')
+		end
+	end,
 })
