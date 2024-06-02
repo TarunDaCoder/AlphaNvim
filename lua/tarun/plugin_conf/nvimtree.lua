d4bd4d21c98572c302a08d0850e0a7d3d04cedd6
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-02 10:21:53 +0530

    update nvimtree conf

--- lua/tarun/plugin_conf/nvimtree.lua
+++ lua/tarun/plugin_conf/nvimtree.lua
@@ -1,33 +1,10 @@
+local tree = require('nvim-tree')
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
-
--- empty setup using defaults
-require('nvim-tree').setup()
-
--- OR setup with some options
-require('nvim-tree').setup({
-	sort = {
-		sorter = 'case_sensitive',
-	},
-	view = {
-		width = 30,
-	},
-	renderer = {
-		group_empty = true,
-	},
-	filters = {
-		dotfiles = true,
-	},
-	actions = {
-		open_file = {
-			quit_on_open = true,
-		},
-	},
-})
 
 -- auto close
 local function is_modified_buffer_open(buffers)
@@ -51,3 +28,80 @@ vim.api.nvim_create_autocmd('BufEnter', {
 		end
 	end,
 })
+
+local options = {
+	filters = {
+		dotfiles = false,
+	},
+	disable_netrw = true,
+	hijack_netrw = true,
+	hijack_cursor = true,
+	hijack_unnamed_buffer_when_opening = false,
+	sync_root_with_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_root = false,
+	},
+	view = {
+		adaptive_size = false,
+		side = 'left',
+		width = 30,
+		preserve_window_proportions = true,
+	},
+	git = {
+		enable = true,
+		ignore = true,
+	},
+	filesystem_watchers = {
+		enable = true,
+	},
+	actions = {
+		open_file = {
+			resize_window = true,
+		},
+	},
+	renderer = {
+		root_folder_label = false,
+		highlight_git = true,
+		highlight_opened_files = 'none',
+
+		indent_markers = {
+			enable = true,
+		},
+
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+
+			glyphs = {
+				default = '󰈚',
+				symlink = '',
+				folder = {
+					default = '',
+					empty = '',
+					empty_open = '',
+					open = '',
+					symlink = '',
+					symlink_open = '',
+					arrow_open = '',
+					arrow_closed = '',
+				},
+				git = {
+					unstaged = '✗',
+					staged = '✓',
+					unmerged = '',
+					renamed = '➜',
+					untracked = '★',
+					deleted = '',
+					ignored = '◌',
+				},
+			},
+		},
+	},
+}
+
+tree.setup(options)
