e7ecd1f286a31811e0786740ddeebd85e7896e84
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-02 14:10:34 +0530

    i don't use trouble anymore

--- lua/tarun/plugin_conf/lualine.lua
+++ lua/tarun/plugin_conf/lualine.lua
@@ -1,23 +1,5 @@
 require('lualine').setup({
 	options = {
         theme = 'onedark',
-        -- theme = require("neofusion.lualine"),
-        function(_, opts)
-            local trouble = require('trouble')
-            local symbols = trouble.statusline({
-                mode = 'lsp_document_symbols',
-                groups = {},
-                title = false,
-                filter = { range = true },
-                format = '{kind_icon}{symbol.name:Normal}',
-                -- The following line is needed to fix the background color
-                -- Set it to the lualine section you want to use
-                hl_group = 'lualine_c_normal',
-            })
-            table.insert(opts.sections.lualine_c, {
-                symbols.get,
-                cond = symbols.has,
-            })
-        end,
 	},
 })
