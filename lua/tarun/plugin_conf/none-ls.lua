77be891d57048d67dea59bea7f973c9ca82f32f9
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-02 14:10:28 +0530

    remove unnecessary comments

--- lua/tarun/plugin_conf/none-ls.lua
+++ lua/tarun/plugin_conf/none-ls.lua
@@ -3,13 +3,10 @@ null.setup({
     sources = {
         -- Javascript
         null.builtins.formatting.prettier,
-        -- null.builtins.diagnostics.eslint,
         require('none-ls.diagnostics.eslint'),
 
         -- Lua
         null.builtins.formatting.stylua,
-        -- null.builtins.diagnostics.luacheck,
-        -- require("none-ls.diagnostics.luacheck"),
         require('none-ls-luacheck.diagnostics.luacheck'),
     },
     auto_start = true,
