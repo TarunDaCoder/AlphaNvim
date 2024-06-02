c0c9ee1c8d6e98028ef16052ca34dce98c75a0dc
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-02 09:54:12 +0530

    fix notify

--- lua/tarun/plugin_conf/notify.lua
+++ lua/tarun/plugin_conf/notify.lua
@@ -1,7 +1,11 @@
 local notify = require('notify')
 
 notify.setup({
-    timeout = 3000,
-    background_color = "#16191f",
-    render = "wrapped-compact",
+	timeout = 3000,
+	-- background_color = '#16191f',
+	render = 'wrapped-compact',
 })
+
+vim.cmd([[
+hi NotifyBackground guibg = #16191f
+]])
