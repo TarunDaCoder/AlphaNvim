9d7cf68cb283d0220ae3d0896030cfb1171b43a0
TarunDaCoder <tarunsnair09@gmail.com>
2024-06-07 21:48:33 +0530

    feat: install fidget

--- lua/tarun/plugins/lspconf.lua
+++ lua/tarun/plugins/lspconf.lua
@@ -2,35 +2,34 @@ local lspconfig = require('lspconfig')
 
 require('mason').setup()
 require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'cssls', 'eslint', 'pyright', },
+	ensure_installed = { 'lua_ls', 'cssls', 'eslint', 'pyright' },
 })
-require("mason-null-ls").setup()
+require('mason-null-ls').setup()
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 lspconfig.lua_ls.setup({
-    capabilties = capabilities,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' },
-            },
-        },
-        workspace = {
-            library = {
-                [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                [vim.fn.stdpath('config') .. '/lua'] = true,
-            },
-        },
-    },
+	capabilties = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' },
+			},
+		},
+		workspace = {
+			library = {
+				[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+				[vim.fn.stdpath('config') .. '/lua'] = true,
+			},
+		},
+	},
 })
 
-
 function on_attach(client, bufnr)
-    local bufopts = { nnoremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	local bufopts = { nnoremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 end
 
 lspconfig.pyright.setup({})
@@ -40,16 +39,18 @@ lspconfig.eslint.setup({})
 
 local signs = { Error = '󰅚 ', Warn = '󰀪 ', Hint = '󰌶 ', Info = ' ' }
 for type, icon in pairs(signs) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = 'DiagnosticSign' .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 vim.diagnostic.config({
-    virtual_text = {
-        prefix = ' ',
-    },
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = false,
+	virtual_text = {
+		prefix = ' ',
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = false,
 })
+
+require('fidget').setup()
