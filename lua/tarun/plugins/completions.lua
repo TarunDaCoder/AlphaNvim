3cdf186df5ce9ab10df16376243fe7357c994a1d
TarunDaCoder <tarunsnair09@gmail.com>
2024-06-10 00:14:53 +0530

    chore: remove scrollbar for cmp

--- lua/tarun/plugins/completions.lua
+++ lua/tarun/plugins/completions.lua
@@ -26,17 +26,15 @@ cmp.setup({
 	window = {
 		completion = {
 			border = border,
-			scrollbar = '║',
 		},
 		documentation = {
 			border = border,
-			scrollbar = '║',
 		},
 	},
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-Space>'] = cmp.mapping.complete({}),
 		['<C-e>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }),
 		['<Tab>'] = cmp.mapping(function(fallback)
