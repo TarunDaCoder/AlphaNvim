5d2c67b9463c02578b97fb84e4d9cbaa9eb0ff07
TarunDaCoder <tarunsnair09@gmail.com>
2024-06-10 20:24:02 +0530

    feat: add nice ascii art at the top of init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
+--           _       _           _   _       _
+--     /\   | |     | |         | \ | |     (_)
+--    /  \  | |_ __ | |__   __ _|  \| |_   ___ _ __ ___
+--   / /\ \ | | '_ \| '_ \ / _` | . ` \ \ / / | '_ ` _ \
+--  / ____ \| | |_) | | | | (_| | |\  |\ V /| | | | | | |
+-- /_/    \_\_| .__/|_| |_|\__,_|_| \_| \_/ |_|_| |_| |_|
+--            | |
+--            |_|
+-- Author: TarunDaCoder (https://github.com/TarunDaCoder)
+-- GitHub: https://github.com/TarunDaCoder/AlphaNvim
+-- License: MIT License Copyright (c) 2024 TarunDaCoder
+
 -- Enable lazy loader
 vim.loader.enable()
 
