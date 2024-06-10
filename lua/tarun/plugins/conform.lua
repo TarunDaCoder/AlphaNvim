6e4f7999ac290d731f04d836bdbd4381c6f171f7
TarunDaCoder <tarunsnair09@gmail.com>
2024-06-10 17:59:37 +0530

    feat: install conform instead of none-ls

---
+++ lua/tarun/plugins/conform.lua
@@ -0,0 +1,10 @@
+require("conform").setup({
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+    formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { 'prettier' },
+    }
+})
