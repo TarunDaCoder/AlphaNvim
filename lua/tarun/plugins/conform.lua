b6a5b707968906fee734d0a32f8acb5e8af6f9b4
TarunDaCoder <tarunsnair09@gmail.com>
2024-06-16 22:05:09 +0530

    feat: add formatting for html

--- lua/tarun/plugins/conform.lua
+++ lua/tarun/plugins/conform.lua
@@ -1,10 +1,11 @@
-require("conform").setup({
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-    },
-    formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { 'prettier' },
-    }
+require('conform').setup({
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+	formatters_by_ft = {
+		lua = { 'stylua' },
+		javascript = { 'prettierd' },
+		html = { 'prettierd' },
+	},
 })
