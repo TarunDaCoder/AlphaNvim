c894698fbc95ce37123b93cbbbe7cba4c0a07da2
TarunDaCoder <tarunsnair09@gmail.com>
2024-06-16 22:15:16 +0530

    updates

--- lua/tarun/plugins/init.lua
+++ lua/tarun/plugins/init.lua
@@ -1,23 +1,35 @@
+-- Statusline and tabline
 require('tarun.plugins.statusline')
+require('tarun.plugins.bufferline')
+
+-- NvimTree
 require('tarun.plugins.nvimtree')
+
+-- Telescope
 require('tarun.plugins.telescope')
+
+-- LSP
 require('tarun.plugins.lspconf')
 require('tarun.plugins.completions')
-require('tarun.plugins.gitsigns')
+require('tarun.plugins.conform')
 require('tarun.plugins.autopairs')
+
+-- Git
+require('tarun.plugins.gitsigns')
+
+-- Treesitter
+require('tarun.plugins.treesitter')
+require('tarun.plugins.autotags')
+
+-- Other stuff
 require('tarun.plugins.dash')
 require('tarun.plugins.neorg')
-require('tarun.plugins.bufferline')
 require('tarun.plugins.indent')
 require('tarun.plugins.oil')
-require('tarun.plugins.conform')
 require('tarun.plugins.colorizer')
 require('tarun.plugins.toggleterm')
 require('tarun.plugins.overseer')
-require('tarun.plugins.notify')
-require('tarun.plugins.autotags')
 require('tarun.plugins.surround')
-require('tarun.plugins.treesitter')
 require('tarun.plugins.live-server')
 require('tarun.plugins.better_escape')
 require('tarun.plugins.supermaven')
