3ec593d1ec6bc634a7bef03d906af1573ad38ce7
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-26 16:05:47 +0530

    feat: change keymaps to whichkey

--- lua/tarun/core/remaps.lua
+++ lua/tarun/core/remaps.lua
@@ -23,8 +23,10 @@ map('n', '<C-Down>', ':resize -2<CR>', opts)
 map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
 map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
--- Visual mode --
+-- Remove hl search
+map('n', '<Esc>', ':nohlsearch<CR>', opts)
 
+-- Visual mode --
 -- Stay in indent mode
 map('v', '<', '<gv', opts)
 map('v', '>', '>gv', opts)
@@ -48,44 +50,6 @@ map('t', '<C-j>', '<C-\\><C-N><C-w>j', term_opts)
 map('t', '<C-k>', '<C-\\><C-N><C-w>k', term_opts)
 map('t', '<C-l>', '<C-\\><C-N><C-w>l', term_opts)
 
--- LSP --
-map('n', '<leader>lr', vim.lsp.buf.rename, { desc = 'Rename' }, opts)
-map('n', '<leader>lD', vim.lsp.buf.declaration, { desc = 'Go to declaration' }, opts)
-map('n', '<leader>ld', vim.lsp.buf.definition, { desc = 'Go to definition' }, opts)
-map('n', '<leader>lh', vim.lsp.buf.hover, { desc = 'Hover' }, opts)
-map('n', '<leader>lt', vim.lsp.buf.type_definition, { desc = 'Go to type definition' }, opts)
-map('n', '<leader>ls', vim.lsp.buf.signature_help, { desc = 'Signature help' }, opts)
-map('n', '<leader>lc', vim.lsp.buf.code_action, { desc = 'Code action' }, opts)
-
 -- Navigate buffers
 map('n', '<TAB>', ':BufferLineCycleNext<CR>', opts)
 map('n', '<S-TAB>', ':BufferLineCyclePrev<CR>', opts)
-
--- PLugin stuff --
--- NvimTree
-map('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Nvim Tree' }, opts)
-
--- Telescope
-map('n', '<leader>tf', ':Telescope find_files<CR>', { desc = 'Find files' }, opts)
-map('n', '<leader>tg', ':Telescope live_grep<CR>', { desc = 'Live grep' }, opts)
-map('n', '<leader>tt', ':Telescope<CR>', { desc = 'Telescope' }, opts)
-map('n', '<leader>th', ':Telescope help_tags<CR>', { desc = 'Help tags' }, opts)
-map('n', '<leader>thl', ':Telescope highlights<CR>', { desc = 'Highlights' }, opts)
-map('n', '<leader>tu', ':Telescope undo<CR>', { desc = 'Undo changes' }, opts)
-
--- Remove hl search
-map('n', '<Esc>', ':nohlsearch<CR>', opts)
-
--- Gitsigns
-map('n', '<leader>gs', ':Gitsigns stage_hunk<CR>', { desc = 'Stage hunk' }, opts)
-map('n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>', { desc = 'Undo stage hunk' }, opts)
-map('n', '<leader>gr', ':Gitsigns reset_hunk<CR>', { desc = 'Reset hunk' }, opts)
-map('n', '<leader>gt', ':Gitsigns reset_buffer<CR>', { desc = 'Reset buffer' }, opts)
-map('n', '<leader>ga', ':Gitsigns stage_buffer<CR>', { desc = 'Stage buffer' }, opts)
-map('n', '<leader>gi', ':Gitsigns toggle_current_line_blame<CR>', { desc = 'Toggle current line blame' }, opts)
-map('n', '<leader>gd', ':Gitsigns diffthis<CR>', { desc = 'Diff this' }, opts)
-map('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', { desc = 'Preview hunk' }, opts)
-map('n', '<leader>gS', ':Gitsigns stage_buffer<CR>', { desc = 'Stage buffer' }, opts)
-map('n', '<leader>gu', ':Gitsigns reset_buffer<CR>', { desc = 'Reset buffer' }, opts)
-map('n', '<leader>gn', ':Gitsigns next_hunk<CR>', { desc = 'Next hunk' }, opts)
-map('n', '<leader>gp', ':Gitsigns prev_hunk<CR>', { desc = 'Previous hunk' }, opts)
--- lua/tarun/plugins/whichkey.lua
+++ lua/tarun/plugins/whichkey.lua
@@ -1,4 +1,5 @@
 local wk = require('which-key')
+local opts = { noremap = true, silent = true }
 
 wk.setup({
 	window = {
@@ -9,5 +10,63 @@ wk.setup({
 		separator = '»',
 		group = '+',
 	},
-	require('tarun.core.remaps'),
+})
+
+wk.register({
+	t = {
+		name = 'Telescope',
+		f = { ':Telescope find_files<CR>', 'Find files', opts },
+		g = { ':Telescope live_grep<CR>', 'Live grep', opts },
+		h = { ':Telescope help_tags<CR>', 'Help tags', opts },
+		s = { ':Telescope highlights<CR>', 'Highlights', opts },
+		u = { ':Telescope undo<CR>', 'Undo changes', opts },
+		t = { ':Telescope<CR>', 'Telescope', opts },
+	},
+	prefix = '<leader>',
+})
+
+wk.register({
+	l = {
+		name = 'LSP',
+		r = { vim.lsp.buf.rename, 'Rename', opts },
+		d = { vim.lsp.goto_definition, 'Goto definition', opts },
+		i = { vim.lsp.buf.implementation, 'Goto implementation', opts },
+		a = { vim.lsp.buf.code_action, 'Code action', opts },
+		f = { vim.lsp.buf.formatting, 'Format', opts },
+		s = { vim.lsp.buf.signature_help, 'Signature help', opts },
+		D = { vim.lsp.buf.declaration, 'Go to declaration', opts },
+		t = { vim.lsp.buf.type_definition, 'Type definition', opts },
+		e = { vim.diagnostic.open_float, 'Show diagnostics', opts },
+		h = { vim.lsp.buf.hover, 'Hover', opts },
+	},
+	prefix = '<leader>',
+})
+
+wk.register({
+	g = {
+		name = 'Gitsigns',
+		s = { ':Gitsigns stage_hunk<CR>', 'Stage hunk', opts },
+		u = { ':Gitsigns undo_stage_hunk<CR>', 'Undo stage hunk', opts },
+		r = { ':Gitsigns reset_hunk<CR>', 'Reset hunk', opts },
+		t = { ':Gitsigns reset_buffer<CR>', 'Reset buffer', opts },
+		a = { ':Gitsigns stage_buffer<CR>', 'Stage buffer', opts },
+		b = { ':Gitsigns reset_buffer<CR>', 'Reset buffer', opts },
+		d = { ':Gitsigns diffthis<CR>', 'Diff this', opts },
+		h = { ':Gitsigns preview_hunk<CR>', 'Preview hunk', opts },
+		n = { ':Gitsigns next_hunk<CR>', 'Next hunk', opts },
+		p = { ':Gitsigns prev_hunk<CR>', 'Previous hunk', opts },
+	},
+	prefix = '<leader>',
+})
+
+wk.register({
+	e = {
+		name = 'NvimTree',
+		e = { ':NvimTreeToggle<CR>', 'Toggle', opts },
+		f = { ':NvimTreeFindFile<CR>', 'Find file', opts },
+		r = { ':NvimTreeRefresh<CR>', 'Refresh', opts },
+		R = { ':NvimTreeRefresh<CR>', 'Refresh', opts },
+		t = { ':NvimTreeToggle<CR>', 'Toggle', opts },
+	},
+	prefix = '<leader>',
 })
