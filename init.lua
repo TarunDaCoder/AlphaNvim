8ff712b54a59958db0d47954f93360cfbbd56aad
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-28 22:25:46 +0530

    speed

--- init.lua
+++ init.lua
@@ -18,3 +18,31 @@ require('tarun.core')
 
 -- Source plugin configs
 require('tarun.plugins')
+
+-- Some secret stuff to reduce my strartup time
+local g = vim.g
+
+g.loaded_gzip = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_2html_plugin = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
+g.loaded_ftplugin = 1
+g.did_load_ftplugin = 1
+
+g.loaded_python3_provider = 1
+g.loaded_python_provider = 1
+g.loaded_ruby_provider = 1
+g.loaded_perl_provider = 1
