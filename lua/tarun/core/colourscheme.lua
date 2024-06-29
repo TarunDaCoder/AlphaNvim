174cc574f7ad15f82d9858fedd94947f07183f38
TarunDaCoder <tarunsnair04@gmail.com>
2024-06-29 10:07:12 +0530

    cust: customize colourscheme

--- lua/tarun/core/colourscheme.lua
+++ lua/tarun/core/colourscheme.lua
@@ -2,6 +2,18 @@
 require('onedarkpro').setup({
 	options = {
 		transparency = true,
+		cursorline = true,
+		highlight_inactive_windows = true,
+	},
+	styles = {
+		comments = 'italic',
+		functions = 'italic',
+		keywords = 'italic',
+		strings = 'italic',
+		variables = 'italic',
+	},
+	plugins = {
+		all = true,
 	},
 })
 
