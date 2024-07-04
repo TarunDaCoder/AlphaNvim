395be0ce1e94374b26220d9c669954941da918b4
TarunDaCoder <tarunsnair09@gmail.com>
2024-07-04 11:38:56 +0000

    feat: add custom highlights

---
+++ lua/tarun/core/highlights.lua
@@ -0,0 +1,8 @@
+local hl = vim.api.nvim_set_hl
+
+hl(0, 'FloatBorder', { bg = '#282c34' })
+hl(0, 'NormalFloat', { bg = '#282c34' })
+hl(0, 'WhichKeyFloat', { bg = '#282c34' })
+
+hl(0, 'TelescopeNormal', { bg = '#2d323b' })
+hl(0, 'TelescopeBorder', { bg = '#2d323b' })
