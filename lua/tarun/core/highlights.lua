17b67ade1d12d32b36c9971c933656ff6f48f075
TarunDaCoder <tarunsnair09@gmail.com>
2024-07-15 12:49:46 +0000

    feat: change nvim-tree highlights

--- lua/tarun/core/highlights.lua
+++ lua/tarun/core/highlights.lua
@@ -6,3 +6,7 @@ hl(0, 'WhichKeyFloat', { bg = '#282c34' })
 
 hl(0, 'TelescopeNormal', { bg = '#2d323b' })
 hl(0, 'TelescopeBorder', { bg = '#2d323b' })
+
+hl(0, 'NvimTreeFolderIcon', { fg = '#afd787' })
+hl(0, 'NvimTreeClosedFolderIcon', { fg = '#afd787' })
+hl(0, 'NvimTreeOpenedFolderIcon', { fg = '#afd787' })
