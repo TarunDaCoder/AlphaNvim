0e5be84a46b602365d41784aaba6eb48ce489838
TarunDaCoder <tarunsnair09@gmail.com>
2024-07-17 11:35:34 +0000

    feat: change the look of telescope

--- lua/tarun/core/highlights.lua
+++ lua/tarun/core/highlights.lua
@@ -10,3 +10,7 @@ hl(0, 'TelescopeBorder', { bg = '#2d323b' })
 hl(0, 'NvimTreeFolderIcon', { fg = '#afd787' })
 hl(0, 'NvimTreeClosedFolderIcon', { fg = '#afd787' })
 hl(0, 'NvimTreeOpenedFolderIcon', { fg = '#afd787' })
+
+hl(0, 'TelescopePreviewTitle', { fg = '#252931', bg = '#98c379' })
+hl(0, 'TelescopePromptTitle', { fg = '#252931', bg = '#e06c75' })
+hl(0, 'TelescopeResultsTitle', { fg = '#252931', bg = '#98c379' })
