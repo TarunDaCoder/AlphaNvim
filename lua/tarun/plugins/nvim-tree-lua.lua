579d3ca2d9e152839e8d33ebcdaf94830f91552e
TarunDaCoder <tarunsnair09@gmail.com>
2024-07-27 04:59:23 +0000

    feat: nvim-tree quit on open

--- lua/tarun/plugins/nvim-tree-lua.lua
+++ lua/tarun/plugins/nvim-tree-lua.lua
@@ -6,29 +6,6 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- auto close
-local function is_modified_buffer_open(buffers)
-	for _, v in pairs(buffers) do
-		if v.name:match('NvimTree_') == nil then
-			return true
-		end
-	end
-	return false
-end
-
-vim.api.nvim_create_autocmd('BufEnter', {
-	nested = true,
-	callback = function()
-		if
-			#vim.api.nvim_list_wins() == 1
-			and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil
-			and is_modified_buffer_open(vim.fn.getbufinfo({ bufmodified = 1 })) == false
-		then
-			vim.cmd('quit')
-		end
-	end,
-})
-
 local options = {
 	filters = {
 		dotfiles = false,
@@ -58,6 +35,7 @@ local options = {
 	actions = {
 		open_file = {
 			resize_window = true,
+			quit_on_open = true,
 		},
 	},
 	renderer = {
