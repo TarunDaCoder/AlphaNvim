8a91cd90d88949cd21495afea0b6ca5a42c9ca9c
TarunDaCoder <tarunsnair09@gmail.com>
2024-07-28 17:40:44 +0000

    Revert "feat: remove ensure installed"
    This reverts commit 3bc3238ed78affa7b7e4aec40ac9f78d99f14604.

--- lua/tarun/plugins/treesitter.lua
+++ lua/tarun/plugins/treesitter.lua
@@ -2,7 +2,8 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 	config = function()
 		require('nvim-treesitter.configs').setup({
-			sync_install = true,
+			ensure_installed = { 'lua', 'html', 'css', 'javascript', 'rust', 'toml', 'yaml', 'bash' },
+			sync_install = false,
 			auto_install = true,
 			highlight = { enable = true },
 			matchup = { enable = true },
