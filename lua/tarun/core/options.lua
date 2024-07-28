6cb8d80de77bac0da8b8a59352a8b1cbdc763bb1
TarunDaCoder <tarunsnair09@gmail.com>
2024-07-28 07:42:11 +0000

    feat: add options

---
+++ lua/tarun/core/options.lua
@@ -0,0 +1,16 @@
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.splitbelow = true
+o.splitright = true
+o.wrap = true
+o.expandtap = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.clipboard = 'unnamedplus'
+o.scrolloff = 999
+o.virtualedit = 'block'
+o.inccommand = 'split'
+o.ignorecase = true
+o.termguicolors = true
