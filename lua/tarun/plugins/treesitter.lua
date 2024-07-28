3bc3238ed78affa7b7e4aec40ac9f78d99f14604
TarunDaCoder <tarunsnair09@gmail.com>
2024-07-28 17:31:26 +0000

    feat: remove ensure installed

--- lua/tarun/plugins/treesitter.lua
+++ lua/tarun/plugins/treesitter.lua
@@ -2,8 +2,7 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 	config = function()
 		require('nvim-treesitter.configs').setup({
-			ensure_installed = { 'lua', 'html', 'css', 'javascript', 'rust', 'toml', 'yaml', 'bash' },
-			sync_install = false,
+			sync_install = true,
 			auto_install = true,
 			highlight = { enable = true },
 			matchup = { enable = true },
