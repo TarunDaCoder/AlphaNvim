0e920bedfae4e927b212353389c2ed9853221de4
TarunDaCoder <tarunsnair09@gmail.com>
2024-08-01 13:08:32 +0000

    feat: add plugins

--- lua/tarun/plugins/extras.lua
+++ lua/tarun/plugins/extras.lua
@@ -3,4 +3,13 @@ return {
 	{ 'VonHeikemen/searchbox.nvim', event = 'VeryLazy' },
 	{ 'olimorris/onedarkpro.nvim', lazy = true },
 	{ 'TarunDaCoder/sus.nvim', event = 'VeryLazy', opts = {} },
+	{ 'OXY2DEV/markview.nvim', ft = 'markdown', opts = {} },
+	{ 'monkoose/neocodeium', event = 'VeryLazy', opts = {} },
+	{ 'barrett-ruth/live-server.nvim', event = 'VeryLazy', opts = {} },
+	{
+		'olrtg/nvim-emmet',
+		keys = {
+			{ 'n', '<leader>xe', require('nvim-emmet').wrap_with_abbreviation, { desc = 'Emmet' } },
+		},
+	},
 }
