b11108411d1c76e1e1e9758627619d82b558eec0
TarunDaCoder <tarunsnair09@gmail.com>
2024-08-11 16:13:46 +0000

    feat: create separate configs for care and cmp

---
+++ lua/tarun/plugins/cmp.lua
@@ -0,0 +1,99 @@
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+local check_backspace = function()
+	local col = vim.fn.col('.') - 1
+	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
+end
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	window = {
+		completion = {
+			border = 'rounded',
+			winhighlight = 'Normal:CmpPmenu,CursorLine:CmpSel',
+		},
+		documentation = {
+			border = 'rounded',
+		},
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete({}),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expandable() then
+				luasnip.expand()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif check_backspace() then
+				fallback()
+			else
+				fallback()
+			end
+		end, {
+			'i',
+			's',
+		}),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, {
+			'i',
+			's',
+		}),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' }, -- For luasnip users.
+	}, {
+		{ name = 'neorg' },
+		{ name = 'supermaven' },
+	}),
+})
+
+-- Set up friendly snippets with luasnip
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' },
+	},
+})
+
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' },
+	}, {
+		{ name = 'cmdline' },
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false },
+})
+
+local lspkind = require('lspkind')
+cmp.setup({
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = 'symbol',
+			maxwidth = 50,
+			symbol_map = {
+				SuperMaven = ' ',
+			},
+		}),
+	},
+})
