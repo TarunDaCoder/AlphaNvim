70e535c6ed9a1c379aef8f7dd061c8a4819a7e97
TarunDaCoder <tarunsnair09@gmail.com>
2024-08-16 16:35:02 +0000

    fix: care config

--- lua/tarun/plugins/care.lua
+++ lua/tarun/plugins/care.lua
@@ -9,10 +9,32 @@ vim.keymap.set('i', '<c-space>', function()
 	require('care').api.complete()
 end)
 
-vim.keymap.set('i', '<cr>', '<Plug>(CareConfirm)')
 vim.keymap.set('i', '<c-e>', '<Plug>(CareClose)')
+vim.keymap.set('i', '<cr>', '<Plug>(CareConfirm)')
 vim.keymap.set('i', '<tab>', '<Plug>(CareSelectNext)')
 vim.keymap.set('i', '<s-tab>', '<Plug>(CareSelectPrev)')
+
+vim.keymap.set('i', '<c-f>', function()
+	if require('care').api.doc_is_open() then
+		require('care').api.scroll_docs(4)
+	else
+		vim.api.nvim_feedkeys(vim.keycode('<c-f>'), 'n', false)
+	end
+end)
+
+vim.keymap.set('i', '<c-d>', function()
+	if require('care').api.doc_is_open() then
+		require('care').api.scroll_docs(-4)
+	else
+		vim.api.nvim_feedkeys(vim.keycode('<c-f>'), 'n', false)
+	end
+end)
+
+-- require('care').setup({
+-- 	ui = {
+-- 		ghost_text = { enabled = false },
+-- 	},
+-- })
 
 vim.api.nvim_create_autocmd('InsertLeave', {
 	callback = function()
