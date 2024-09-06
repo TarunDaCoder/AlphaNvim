68fb63b4569dc660cd18fe7fd2e9742899e2c875
TarunDaCoder <tarunsnair09@gmail.com>
2024-09-06 17:37:32 +0000

    feat: make some styles italic

--- lua/tarun/core/colourscheme.lua
+++ lua/tarun/core/colourscheme.lua
@@ -8,6 +8,21 @@ require('onedarkpro').setup({
 	plugins = {
 		all = true,
 	},
+	styles = {
+		types = 'NONE',
+		methods = 'NONE',
+		numbers = 'NONE',
+		strings = 'NONE',
+		comments = 'italic',
+		keywords = 'bold,italic',
+		constants = 'NONE',
+		functions = 'italic',
+		operators = 'NONE',
+		variables = 'NONE',
+		parameters = 'NONE',
+		conditionals = 'italic',
+		virtual_text = 'bold,italic',
+	},
 })
 
 vim.cmd([[colorscheme onedark_vivid]])
