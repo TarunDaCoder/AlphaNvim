1f329f61b7ca78881b01cb051219bc728fcaddc6
TarunDaCoder <tarunsnair09@gmail.com>
2024-09-21 17:30:30 +0000

    feat: use rustfmt and superhtml for rust and html respectively

--- lua/tarun/plugins/conform.lua
+++ lua/tarun/plugins/conform.lua
@@ -7,6 +7,7 @@ require('conform').setup({
 	formatters_by_ft = {
 		lua = { 'stylua' },
 		javascript = { 'prettierd' },
-		html = { 'prettierd' },
+		html = { 'superhtml' },
+		rust = { 'rustfmt' },
 	},
 })
