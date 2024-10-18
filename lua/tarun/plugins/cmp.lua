95bba39d2fcba439661579b39cffdd39df313cf0
TarunDaCoder <tarunsnair09@gmail.com>
2024-10-18 18:04:31 +0000

    ref: remove this useless line

--- lua/tarun/plugins/cmp.lua
+++ lua/tarun/plugins/cmp.lua
@@ -34,8 +34,6 @@ cmp.setup({
 				luasnip.expand()
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
-			elseif check_backspace() then
-				fallback()
 			else
 				fallback()
 			end
