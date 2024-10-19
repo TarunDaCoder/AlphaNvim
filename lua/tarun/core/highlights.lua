3f662f18955b3b1e201e647fc0184147175dd20d
TarunDaCoder <tarunsnair09@gmail.com>
2024-10-19 17:22:34 +0000

    fix: FloatBorder highlight

--- lua/tarun/core/highlights.lua
+++ lua/tarun/core/highlights.lua
@@ -39,3 +39,5 @@ hl(0, 'IndentLineCurrent', { fg = '#91d7e3', bg = 'none' })
 
 hl(0, '@care.type.File', { fg = '#F5E0DD', bg = 'none' })
 hl(0, '@care.border', { fg = '#1e2031' })
+
+hl(0, 'FloatBorder', { fg = '#1e2030', bg = '#1e2030' })
