93f5c609eccddeaaead12cd877acf0cded94db68
TarunDaCoder <tarunsnair09@gmail.com>
2024-11-03 06:45:06 +0000

    feat: use neorg icon for norg files

---
+++ lua/tarun/plugins/web-devicons.lua
@@ -0,0 +1,9 @@
+require('nvim-web-devicons').setup({
+    override = {
+        norg = {
+            icon = ' ', -- This icon probably wont be visible in your browser, but insert your icon here.
+            color = '#4878be', -- Neorgs official color
+            name = 'neorg',
+        },
+    },
+})
