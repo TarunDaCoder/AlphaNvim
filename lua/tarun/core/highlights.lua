142725d86c6ffff2f2f491d100863fe056a94395
TarunDaCoder <tarunsnair09@gmail.com>
2024-11-03 07:09:08 +0000

    chore: remove useless highlights

--- lua/tarun/core/highlights.lua
+++ lua/tarun/core/highlights.lua
@@ -1,31 +1,6 @@
 local hl = vim.api.nvim_set_hl
 
--- hl(0, 'FloatBorder', { bg = '#181826' })
--- hl(0, 'NormalFloat', { bg = '#2d323b' })
--- hl(0, 'SignColumn', { bg = '#2d323b' })
--- hl(0, 'LineNr', { bg = '#2d323b' })
--- hl(0, 'LineNrAbove', { fg = '#485061' })
--- hl(0, 'LineNrBelow', { fg = '#485061' })
--- hl(0, 'Normal', { bg = '#2d323b' })
---
 hl(0, 'CmpSel', { bg = '#485061' })
---
--- hl(0, 'NonText', { bg = '#2d323b', fg = '#4a505b' })
---
--- hl(0, 'TelescopeNormal', { bg = '#2d323b' })
--- hl(0, 'TelescopeBorder', { bg = '#2d323b' })
---
--- hl(0, 'NvimTreeFolderIcon', { fg = '#afd787' })
--- hl(0, 'NvimTreeClosedFolderIcon', { fg = '#afd787' })
--- hl(0, 'NvimTreeOpenedFolderIcon', { fg = '#afd787' })
---
--- hl(0, 'TelescopePreviewTitle', { fg = '#252931', bg = '#98c379' })
--- hl(0, 'TelescopePromptTitle', { fg = '#252931', bg = '#e06c75' })
--- hl(0, 'TelescopeResultsTitle', { fg = '#252931', bg = '#98c379' })
---
--- hl(0, 'StatusLine', { bg = '#2d323b' })
---
--- hl(0, 'FoldColumn', { bg = '#2d323b', fg = '#495162' })
 
 hl(0, 'WhichKeyNormal', { bg = '#202334' })
 hl(0, 'WhichKeyBorder', { bg = '#202334' })
